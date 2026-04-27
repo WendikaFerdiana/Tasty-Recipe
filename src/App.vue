757142f804917d532f49f288f9e15edf641f05dd
Wendika <putuwendi2504@gmail.com>
2026-04-27 23:34:05 +0800

    [4] Done

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,22 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div>
+    <web-header></web-header>
+    <home-page></home-page>
+    <web-footer></web-footer>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import WebHeader from "./components/header/WebHeader.vue"
+import HomePage from "./components/pages/HomePage.vue"
+import WebFooter from "./components/footer/WebFooter.vue"
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+export default {
+  components : {
+    WebHeader,
+    HomePage,
+    WebFooter
   }
 }
-</style>
+</script>
---
+++ src/components/pages/HomePage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container-md my-5 py-5">
+        <div class="recipe_title text-center">
+            <h2>All Recipe</h2>
+            <p style="font-size: 15px">
+                Find and share everyday cooking inspiration on
+                Allrecipes. Discover recipes, cooks, videos, and
+                how-tos based on the food you love and the friends
+                you follow.
+            </p>
+        </div>
+        
+        <RecipeList :recipes="recipeData" />
+        
+    </div>
+</template>
+
+<script setup>
+
+import RecipeList from "../recipe/RecipeList.vue";
+
+</script>
